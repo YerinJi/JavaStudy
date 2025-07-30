f0f6d9a40ff7cf72e984473f0a17ba1aa5cb80a3
yerinJi <yerin6428@gmail.com>
2025-07-30 09:14:59 +0900

    01_java 삭제

--- javaBasic/src/com/multi/homework0725/HomeworkEx10.java
+++ javaBasic/src/com/multi/homework0725/HomeworkEx10.java
@@ -13,8 +13,8 @@ public class HomeworkEx10 {
                 }
             }
         }
-        for(int j=0;j<arr.length;j++) {
-            System.out.print(arr[j]+" ");
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
     }
 
