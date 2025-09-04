7e2f6d338136fec38d70b03d08f5f5853d5010da
yerinJi <yerin6428@gmail.com>
2025-09-04 17:37:04 +0900

    @AllArgsConstructor, @NoArgsConstructor 추가

--- servletWork/dynamic-web/src/main/java/com/multi/dto/BoardDTO.java
+++ servletWork/dynamic-web/src/main/java/com/multi/dto/BoardDTO.java
@@ -1,15 +1,18 @@
 package com.multi.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 @Data  //getter,setter,toString
+@AllArgsConstructor
+@NoArgsConstructor
 public class BoardDTO {
     private int id;
     private String title;
     private String content;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-
 
 }
