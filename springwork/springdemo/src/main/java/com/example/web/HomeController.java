8270dff6f08338ed0766b7cecb7efe5a0749018c
yerinJi <yerin6428@gmail.com>
2025-09-05 16:47:16 +0900

    spring legacy 세팅

---
+++ springwork/springdemo/src/main/java/com/example/web/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/home"})
+    public String home(Model model){
+        model.addAttribute("msg", "Spring Legacy (XML) running!");
+        return "home"; // /WEB-INF/views/home.jsp
+    }
+}
+
