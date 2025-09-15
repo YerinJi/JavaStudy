14210ee4d73211d65e74a820704592c516ec9d6b
yerinJi <yerin6428@gmail.com>
2025-09-15 21:21:14 +0900

    DI: xml,annotation 과제

---
+++ springwork/BootAssignment0911/src/main/java/com/multi/BootAssignment0911Application.java
@@ -0,0 +1,13 @@
+package com.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAssignment0911Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootAssignment0911Application.class, args);
+    }
+
+}
---
+++ springwork/BootAssignment0911/src/test/java/com/multi/bootassignment0911/BootAssignment0911ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.bootassignment0911;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAssignment0911ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
